6a0ae32cf4f636e45bd185cffc27e970d73ccdd9
David Berlin <david@stackbit.com>
2024-04-02 13:31:46 +0300

    remove auth

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,10 +16,10 @@ module.exports = {
       resolve: `gatsby-source-drupal`,
       options: {
         baseUrl: process.env.DRUPAL_BASE_URL,
-        basicAuth: {
-          username: process.env.DRUPAL_BASIC_AUTH_USERNAME,
-          password: process.env.DRUPAL_BASIC_AUTH_PASSWORD,
-        },
+        // basicAuth: {
+        //   username: process.env.DRUPAL_BASIC_AUTH_USERNAME,
+        //   password: process.env.DRUPAL_BASIC_AUTH_PASSWORD,
+        // },
         fastBuilds: true,
       },
     },
